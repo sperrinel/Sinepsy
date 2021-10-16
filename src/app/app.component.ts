7396016dde52a6f6005d054c5b204a5693e958d3
sperrinel <s.perrinel@orange.fr>
2021-10-16 12:21:29 +0200

    Bootstrap + Firebase + preparation du projet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Altagama';
+  titre = 'Altagama';
+
+  constructor() {
+    var firebaseConfig = {
+      apiKey: "AIzaSyDgBS61L9tUDYmWpC7-n_Kqgzwp58Ihe4k",
+      authDomain: "altagama-9d227.firebaseapp.com",
+      databaseURL:'https://altagama-9d227-default-rtdb.europe-west1.firebasedatabase.app/',
+      projectId: "altagama-9d227",
+      storageBucket: "altagama-9d227.appspot.com",
+      messagingSenderId: "460364418958",
+      appId: "1:460364418958:web:dfefa6cfa9029efbf91edf",
+      measurementId: "G-0K35M0GR3D"
+    };
+
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    firebase.analytics();
+  }
+
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { AccueilComponent } from './components/accueil/accueil.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: 'accueil', component: AccueilComponent }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    AccueilComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes, { scrollPositionRestoration: 'enabled' })
   ],
   providers: [],
   bootstrap: [AppComponent]
